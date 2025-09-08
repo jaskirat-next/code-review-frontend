785defa37dc7bafab77bdf8556eff90d27417c97
jaskirat-next <zjaskiratsingh@gmail.com>
2025-09-08 10:47:39 +0530

    added vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   const [code , setCode] = useState(`function sum() {
-    return 1+ 1}`)
+    return 1+ 1 + 1}`)
 
   useEffect( () => {
     prism.highlightAll()
